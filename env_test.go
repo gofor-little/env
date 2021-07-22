42384e049b38db311a956d903c1419358a926307
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-07-22 11:41:55 +1000

    feat: cleaned up tests (#7)
    - Cleaned up tests.
    - Added semantic.yml.
    - Added pull request template.
    - Updated dependabot.yml.
    - Updated stale.yml.
    - Updated .gitignore.

--- env_test.go
+++ env_test.go
@@ -5,87 +5,52 @@ import (
 	"testing"
 
 	"github.com/gofor-little/env"
+	"github.com/matryer/is"
 )
 
 func TestLoad(t *testing.T) {
-	if err := env.Load("test-data/test-1.env", "test-data/test-2.env"); err != nil {
-		t.Fatalf("failed to load .env files: %v", err)
-	}
+	is := is.New(t)
+	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
-	if env.Get("PUBLIC_KEY", "") != os.Getenv("PUBLIC_KEY") {
-		t.Fatalf("unexpected result from PUBLIC_KEY environment variable: %s", os.Getenv("PUBLIC_KEY"))
-	}
-
-	if env.Get("PRIVATE_KEY", "") != os.Getenv("PRIVATE_KEY") {
-		t.Fatalf("unexpected result from PRIVATE_KEY environment variable: %s", os.Getenv("PRIVATE_KEY"))
-	}
-
-	if env.Get("DB_NAME", "") != os.Getenv("DB_NAME") {
-		t.Fatalf("unexpected result from DB_NAME environment variable: %s", os.Getenv("DB_NAME"))
-	}
-
-	if env.Get("DB_PASSWORD", "") != os.Getenv("DB_PASSWORD") {
-		t.Fatalf("unexpected result from DB_PASSWORD environment variable: %s", os.Getenv("DB_PASSWORD"))
-	}
+	is.True(env.Get("PUBLIC_KEY", "") == os.Getenv("PUBLIC_KEY"))
+	is.True(env.Get("PRIVATE_KEY", "") == os.Getenv("PRIVATE_KEY"))
+	is.True(env.Get("DB_NAME", "") == os.Getenv("DB_NAME"))
+	is.True(env.Get("DB_PASSWORD", "") == os.Getenv("DB_PASSWORD"))
 }
 
 func TestWrite(t *testing.T) {
-	if err := env.Write("PUBLIC_KEY_NEW", "\"public\\nkey\\nnew\"", "test-data/test-1.env", true); err != nil {
-		t.Fatalf("failed to write to .env file: %v", err)
-	}
+	is := is.New(t)
 
-	if err := env.Write("DB_PASSWORD", "db_password_new", "test-data/test-2.env", true); err != nil {
-		t.Fatalf("failed to write to .env file: %v", err)
-	}
+	is.NoErr(env.Write("PUBLIC_KEY_NEW", "\"public\\nkey\\nnew\"", "test-data/test-1.env", true))
+	is.NoErr(env.Write("DB_PASSWORD", "db_password_new", "test-data/test-2.env", true))
 }
 
 func TestGet(t *testing.T) {
-	if err := env.Load("test-data/test-1.env", "test-data/test-2.env"); err != nil {
-		t.Fatalf("failed to load .env files: %v", err)
-	}
+	is := is.New(t)
+	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
-	value := env.Get("DB_NAME", "db_name_default")
-	if value != "db_name" {
-		t.Fatalf("unexpected result from DB_NAME environment variable: %s", value)
-	}
-
-	value = env.Get("FAKE_KEY", "db_name_default")
-	if value != "db_name_default" {
-		t.Fatalf("unexpected result from FAKE_KEY environment variable: %s", value)
-	}
+	is.True(env.Get("DB_NAME", "db_name_default") == "db_name")
+	is.True(env.Get("FAKE_KEY", "db_name_default") == "db_name_default")
 }
 
 func TestMustGet(t *testing.T) {
-	if err := env.Load("test-data/test-1.env", "test-data/test-2.env"); err != nil {
-		t.Fatalf("failed to load .env files: %v", err)
-	}
+	is := is.New(t)
+	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
 	value, err := env.MustGet("DB_NAME")
-	if err != nil {
-		t.Fatalf("failed to get environment variable for key DB_NAME: %v", err)
-	}
-
-	if value != "db_name" {
-		t.Fatalf("unexpected result from DB_NAME environment variable: %s", value)
-	}
+	is.NoErr(err)
+	is.True(value == "db_name")
 
 	_, err = env.MustGet("FAKE_KEY")
-	if err == nil {
-		t.Fatalf("failed to get environment variable for key FAKE_KEY: %v", err)
-	}
+	is.True(err != nil)
 }
 
 func TestSet(t *testing.T) {
-	if err := env.Load("test-data/test-1.env", "test-data/test-2.env"); err != nil {
-		t.Fatalf("failed to load .env files: %v", err)
-	}
+	is := is.New(t)
+	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
-	if err := env.Set("DB_NAME", "db_name_override"); err != nil {
-		t.Fatalf("failed to set environment variable for key DB_NAME: %v", err)
-	}
+	is.NoErr(env.Set("DB_NAME", "db_name_override"))
 
 	value := os.Getenv("DB_NAME")
-	if value != "db_name_override" {
-		t.Fatalf("unexpected result from DB_NAME environment variable: %s", value)
-	}
+	is.True(value == "db_name_override")
 }
