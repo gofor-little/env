340a7906789c56943aa43e7405dfc79dc2d0d955
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-08-21 11:33:13 +1000

    deps: added go 1.17 support

---
+++ CHANGELOG.md
@@ -0,0 +1,63 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## v1.0.1 - 2021-08-21
+### Added
+* Added Go 1.17 support.
+* Added a changelog.
+* Added a code of conduct.
+
+## v1.0.0 - 2021-06-22
+### Added
+* Added Semantic Pull Requests configuration file.
+* Added a pull request template.
+
+## v0.4.4 - 2021-02-20
+### Added
+* Added Go 1.16 support.
+
+## v0.4.3 - 2020-12-31
+### Changed
+* Updated readme.
+
+## v0.4.2 - 2020-12-30
+### Added
+* Added GitHub Stale action.
+
+## v0.4.1 - 2020-12-30
+### Changed
+* Formatted several files.
+
+## v0.4.0 - 2020-11-03
+### Added
+* Added Go 1.15 support.
+
+### Fixed
+* Fixed a bug not allowing quotes to be written to file when the ```Write``` function was called.
+
+## v0.3.1 - 2020-09-03
+### Changed
+* Updated tests.
+
+## v0.3.0 - 2020-09-03
+### Changed
+* **BREAKING**: Added ```setAfterWrite``` parameter to ```Write``` function.
+
+## v0.2.0 - 2020-08-16
+### Added
+* Added ```Get```, ```MustGet``` and ```Set``` functions.
+
+## v0.1.2 - 2020-06-15
+### Fixed
+* Fixed uncaught error.
+
+## v0.1.1 - 2020-06-11
+### Added
+* Added tests.
+
+## v0.1.0 - 2020-06-10
+### Added
+* Added ```Load``` and ```Write``` functions.
--- env_test.go
+++ env_test.go
@@ -5,52 +5,45 @@ import (
 	"testing"
 
 	"github.com/gofor-little/env"
-	"github.com/matryer/is"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLoad(t *testing.T) {
-	is := is.New(t)
-	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
+	require.NoError(t, env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
-	is.True(env.Get("PUBLIC_KEY", "") == os.Getenv("PUBLIC_KEY"))
-	is.True(env.Get("PRIVATE_KEY", "") == os.Getenv("PRIVATE_KEY"))
-	is.True(env.Get("DB_NAME", "") == os.Getenv("DB_NAME"))
-	is.True(env.Get("DB_PASSWORD", "") == os.Getenv("DB_PASSWORD"))
+	require.True(t, env.Get("PUBLIC_KEY", "") == os.Getenv("PUBLIC_KEY"))
+	require.True(t, env.Get("PRIVATE_KEY", "") == os.Getenv("PRIVATE_KEY"))
+	require.True(t, env.Get("DB_NAME", "") == os.Getenv("DB_NAME"))
+	require.True(t, env.Get("DB_PASSWORD", "") == os.Getenv("DB_PASSWORD"))
 }
 
 func TestWrite(t *testing.T) {
-	is := is.New(t)
-
-	is.NoErr(env.Write("PUBLIC_KEY_NEW", "\"public\\nkey\\nnew\"", "test-data/test-1.env", true))
-	is.NoErr(env.Write("DB_PASSWORD", "db_password_new", "test-data/test-2.env", true))
+	require.NoError(t, env.Write("PUBLIC_KEY_NEW", "\"public\\nkey\\nnew\"", "test-data/test-1.env", true))
+	require.NoError(t, env.Write("DB_PASSWORD", "db_password_new", "test-data/test-2.env", true))
 }
 
 func TestGet(t *testing.T) {
-	is := is.New(t)
-	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
+	require.NoError(t, env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
-	is.True(env.Get("DB_NAME", "db_name_default") == "db_name")
-	is.True(env.Get("FAKE_KEY", "db_name_default") == "db_name_default")
+	require.True(t, env.Get("DB_NAME", "db_name_default") == "db_name")
+	require.True(t, env.Get("FAKE_KEY", "db_name_default") == "db_name_default")
 }
 
 func TestMustGet(t *testing.T) {
-	is := is.New(t)
-	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
+	require.NoError(t, env.Load("test-data/test-1.env", "test-data/test-2.env"))
 
 	value, err := env.MustGet("DB_NAME")
-	is.NoErr(err)
-	is.True(value == "db_name")
+	require.NoError(t, err)
+	require.True(t, value == "db_name")
 
 	_, err = env.MustGet("FAKE_KEY")
-	is.True(err != nil)
+	require.True(t, err != nil)
 }
 
 func TestSet(t *testing.T) {
-	is := is.New(t)
-	is.NoErr(env.Load("test-data/test-1.env", "test-data/test-2.env"))
-
-	is.NoErr(env.Set("DB_NAME", "db_name_override"))
+	require.NoError(t, env.Load("test-data/test-1.env", "test-data/test-2.env"))
+	require.NoError(t, env.Set("DB_NAME", "db_name_override"))
 
 	value := os.Getenv("DB_NAME")
-	is.True(value == "db_name_override")
+	require.True(t, value == "db_name_override")
 }
